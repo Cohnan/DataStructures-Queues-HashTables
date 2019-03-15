379732852f058ecba9722e9980e071f3da215252
Cohnan <sc.puerto10@uniandes.edu.co>
2019-03-15 13:10:12 -0500

    Empezadas clases de pruebas unitarias

---
+++ test/model/data_structures/PruebaColaPrioridad.java
@@ -0,0 +1,19 @@
+package model.data_structures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PruebaColaPrioridad {
+
+	@Test
+	public void testAgregar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelMax() {
+		fail("Not yet implemented");
+	}
+
+}
