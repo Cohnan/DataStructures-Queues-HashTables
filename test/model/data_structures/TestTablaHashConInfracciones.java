724fba1c7e7a8125177e2fc6109ebde184e1f2d6
Cohnan <sc.puerto10@uniandes.edu.co>
2019-03-18 09:06:45 -0500

    Arreglo setUpEscenario()

--- test/model/data_structures/TestTablaHashConInfracciones.java
+++ test/model/data_structures/TestTablaHashConInfracciones.java
@@ -11,9 +11,9 @@ public class TestTablaHashConInfracciones {
 	/**
 	 * Atributos
 	 */
-	private static LinProbTH<Integer, IArregloDinamico<VOMovingViolation>> thLinProb;
+	private static LinProbTH<Integer, IArregloDinamico<VOMovingViolation>> tabla1;
 	
-	private static SepChainTH<Integer, IArregloDinamico<VOMovingViolation>> thSepChain;
+	private static SepChainTH<Integer, IArregloDinamico<VOMovingViolation>> tabla2;
 	
 	IArregloDinamico<Integer> nCargas;
 	
@@ -22,8 +22,18 @@ public class TestTablaHashConInfracciones {
 	 * @param tabla
 	 * @param capInicial
 	 */
-	private void setUpEscenario(ITablaHash<Integer, IArregloDinamico<VOMovingViolation>> tabla, int capInicial ) {
-		this.nCargas = LoadMovingViolations.loadMovingViolations(1, new ITablaHash[] {tabla});
+	private void setUpEscenario(int nTabla, int capInicial ) {
+		if (nTabla == 1) {
+			tabla1 = new LinProbTH<Integer, IArregloDinamico<VOMovingViolation>>(capInicial);
+			tabla2 = null;
+			
+			this.nCargas = LoadMovingViolations.loadMovingViolations(1, new ITablaHash[] {tabla1});
+		} else if (nTabla == 2) {
+			tabla2 = new SepChainTH<Integer, IArregloDinamico<VOMovingViolation>>(capInicial);
+			tabla1 = null;
+			
+			this.nCargas = LoadMovingViolations.loadMovingViolations(1, new ITablaHash[] {tabla2});
+		}
 	}
 	
 	/**
